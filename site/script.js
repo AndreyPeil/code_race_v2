b86e23c42fa4883fbc06ce700710f5de4f1acbc8
Nicolas-SFranca <nicolasdifranc@gmail.com>
2024-08-31 02:37:17 -0300

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    var dropdown = document.getElementById("drop_down");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
